524e96db1d8aa792655508ab524e4df213d42da4
BJ Dedushaj <bjdedushaj@yahoo.com>
2025-02-07 01:04:22 -0600

    nav and hero cta

---
+++ scripts/scripts.js
@@ -0,0 +1,31 @@
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute("href");
+        const targetElement = document.querySelector(targetId);
+        const navHeight = document.querySelector("nav").offsetHeight;
+
+        if (targetElement) {
+            // Calculate the position to scroll to
+            const targetPosition =
+                targetElement.getBoundingClientRect().top +
+                window.scrollY -
+                navHeight;
+
+            // Smooth scroll to the target position
+            window.scrollTo({
+                top: targetPosition,
+                behavior: "smooth",
+            });
+        }
+    });
+});
+
+// Document Ready
+document.addEventListener("DOMContentLoaded", function () {
+    //console.log("ready!");
+
+    AOS.init();
+});
