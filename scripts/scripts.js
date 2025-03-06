0cc7d5f412fab34211c73c3032defa6ba68d18aa
BJ <bjdedushaj@gmail.com>
2025-03-06 01:52:30 -0600

    all general sections complete

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,9 +4,7 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
         e.preventDefault();
 
         const targetId = this.getAttribute("href");
-        console.log(targetId);
         const targetElement = document.querySelector(targetId);
-        console.log(targetElement);
         const navHeight = document.querySelector("nav").offsetHeight;
 
         if (targetElement) {
@@ -15,8 +13,6 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
                 targetElement.getBoundingClientRect().top +
                 window.scrollY -
                 navHeight;
-
-            console.log(targetPosition);
 
             // Smooth scroll to the target position
             window.scrollTo({
