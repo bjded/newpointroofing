fbcfcb3356ae3a2f7581260276d40d964406e786
BJ <bjdedushaj@gmail.com>
2025-03-09 15:04:43 -0500

    favicon added

--- php/head.php
+++ php/head.php
@@ -2,9 +2,17 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <!-- Temporary No Follow -->
-<meta name="robots" content="noindex, nofollow" />
+<!-- <meta name="robots" content="noindex, nofollow" /> -->
 
 <title><?= !empty($page_title) ? $page_title : "Home"; ?> :: New Point Roofing, LLC | newpointroofing.net</title>
+
+<!-- Favicon -->
+<link rel="icon" type="image/png" href="images/favicon/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="images/favicon/favicon.svg" />
+<link rel="shortcut icon" href="images/favicon/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="New Point Roofing" />
+<link rel="manifest" href="images/favicon/site.webmanifest" />
 
 <!-- Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com" />
