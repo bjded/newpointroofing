56eadada8b2aeea7b9eced6d53a17034cf646cc1
BJ <bjdedushaj@gmail.com>
2025-03-10 12:45:05 -0500

    showcase link fix

--- php/showcase.php
+++ php/showcase.php
@@ -3,7 +3,7 @@
         <h2 class="container-heading">Digital Showcase</h2>
         <div class="image" data-aos="fade-up" data-aos-duration="500">
             <a
-                href="images/showcase/showcase1.png"
+                href="images/showcase/showcase1.webp"
                 class="btn btn-primary" data-fslightbox="showcase">View Larger Image</a>
         </div>
         <div
@@ -12,7 +12,7 @@
             data-aos-duration="500"
             data-aos-delay="250">
             <a
-                href="images/showcase/showcase4.png"
+                href="images/showcase/showcase4.webp"
                 class="btn btn-primary" data-fslightbox="showcase">View Larger Image</a>
         </div>
         <div
@@ -21,7 +21,7 @@
             data-aos-duration="500"
             data-aos-delay="500">
             <a
-                href="images/showcase/showcase5.png"
+                href="images/showcase/showcase5.webp"
                 class="btn btn-primary" data-fslightbox="showcase">View Larger Image</a>
         </div>
         <div
@@ -30,7 +30,7 @@
             data-aos-duration="500"
             data-aos-delay="500">
             <a
-                href="images/showcase/showcase2.png"
+                href="images/showcase/showcase2.webp"
                 class="btn btn-primary" data-fslightbox="showcase">View Larger Image</a>
         </div>
         <div
@@ -39,12 +39,12 @@
             data-aos-duration="500"
             data-aos-delay="250">
             <a
-                href="images/showcase/showcase3.png"
+                href="images/showcase/showcase3.webp"
                 class="btn btn-primary" data-fslightbox="showcase">View Larger Image</a>
         </div>
         <div class="image" data-aos="fade-up" data-aos-duration="500">
             <a
-                href="images/showcase/showcase6.png"
+                href="images/showcase/showcase6.webp"
                 class="btn btn-primary" data-fslightbox="showcase">View Larger Image</a>
         </div>
     </div>
